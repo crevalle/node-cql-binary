aca1c321387aee60e3d158794c1179c305e910a4
Desmond Bowe <desmondbowe@gmail.com>
2013-03-17 16:01:51 -0400

    Working test for creating a keyspace
    No assertions on the query response yet, because we haven't made that
    work yet.

--- test/tests/index_tests.js
+++ test/tests/index_tests.js
@@ -1,12 +1,42 @@
 var vows = require('vows'),
-    expect = require('expect.js');
+    expect = require('expect.js'),
+    cqlbinary = require('../../lib/cqlbinary/index');
 
 vows.describe('cqlbinary').addBatch({
-  'connect': {
-    topic: 1,
+  'Connection': {
+    topic: function () {
+      var callback = this.callback;
+      cqlbinary.connect(function (error, connection) {
+        callback(undefined, connection);
+      });
+    },
 
-    'math should be correct': function (topic) {
-      expect(topic).to.be(1);
+    'it should connect': function (error, connection) {
+      expect(connection).to.be.ok();
+    },
+
+    'with keyspace': {
+      topic: function (connection) {
+        connection.execute(
+          "CREATE KEYSPACE binarytest WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}",
+          cqlbinary.CONSISTENCY.ONE,
+          this.callback
+        );
+      },
+
+      'it should send response': function (error, response) {
+        expect(error).not.to.be.ok();
+        expect(response).to.be.ok();
+      },
+
+      'teardown': function () {
+        connection = arguments[arguments.length - 1];
+        connection.execute("DROP KEYSPACE binarytest", cqlbinary.CONSISTENCY.ONE);
+      }
+    },
+
+    'teardown': function (connection) {
+      connection.disconnect();
     }
-  }
+  },
 }).run();
